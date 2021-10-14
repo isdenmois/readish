95fe1e632aed97beedd2eb6a72cac1341d722560
Denis Moiseev <isdenmois@gmail.com>
2021-10-14 21:23:40 +0300

    Made QR larger

--- app/src/main/java/com/isdenmois/readish/screens/transfers/TransfersScreen.kt
+++ app/src/main/java/com/isdenmois/readish/screens/transfers/TransfersScreen.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -28,7 +30,7 @@ fun TransfersScreen() {
             .background(Color.White),
     ) {
         if (qr != null) {
-            Image(bitmap = qr!!, contentDescription = "")
+            Image(bitmap = qr!!, contentDescription = address, modifier = Modifier.size(512.dp))
         }
 
         Text(address, fontSize = 32.sp)
